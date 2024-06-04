e233c7b6110537b2cd06bdc9c568c76d970f30dd
Antoine <antoine.roy@tetras-libre.fr>
2024-06-04 12:13:22 +0200

    wip module wire

--- src/features/ModuleWIre/ModuleWire.tsx
+++ src/features/ModuleWIre/ModuleWire.tsx
@@ -1,5 +1,6 @@
 import {NavBar} from "../../Components/NavBar.tsx";
 import {useCallback, useState} from "react";
+import './moduleWire.css';
 
 export const ModuleWire = ()=>{
     const [nbrWire, setNbrWire] = useState(0)
@@ -11,10 +12,13 @@ export const ModuleWire = ()=>{
         <>
             <NavBar/>
             <div>
-                <button onClick={()=>handleNmbrWire(3)}>3</button>
-                <button onClick={()=>handleNmbrWire(4)}>4</button>
-                <button onClick={()=>handleNmbrWire(5)}>5</button>
-                <button onClick={()=>handleNmbrWire(6)}>6</button>
+                <p>Combien de fil y a t-il ?</p>
+                <div className='button-nbrwire-container'>
+                    <button onClick={()=>handleNmbrWire(3)}>3</button>
+                    <button onClick={()=>handleNmbrWire(4)}>4</button>
+                    <button onClick={()=>handleNmbrWire(5)}>5</button>
+                    <button onClick={()=>handleNmbrWire(6)}>6</button>
+                </div>
             </div>
             {nbrWire === 1 && (
                 <>
